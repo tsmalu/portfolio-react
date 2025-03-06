882eaaadc2e3aed8a09ebe1c9b50c1b6f5bfa3a0
Malu <malu@MacBook-Pro-de-Malu.local>
2025-03-06 15:42:16 -0300

    Build corrigido para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,16 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/portfolio-react/",
+  base: "/portfolio-react/", // Caminho para GitHub Pages
   plugins: [react()],
   build: {
-    outDir: 'dist',
-    emptyOutDir: true,
-  }
+    outDir: "dist",
+    assetsDir: "assets",
+    rollupOptions: {
+      input: {
+        main: "index.html",
+      },
+    },
+  },
 });
